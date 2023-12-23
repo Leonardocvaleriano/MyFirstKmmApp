65d0818541c31969fed7aa36eb6b0b1f3b5c5fe4
Leonardocvaleriano <leonardoconsanivaleriano@gmail.com>
2023-12-23 13:24:07 +0000

    feat: implemented list of strings

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -2,11 +2,14 @@ import SwiftUI
 import shared
 
 struct ContentView: View {
-	let greet = Greeting().greet()
-
-	var body: some View {
-		Text(greet)
-	}
+    let phrases = Greeting().greet()
+    
+    var body: some View {
+        List(phrases, id: \.self) {
+            Text($0)
+        }
+        
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
--- shared/src/commonMain/kotlin/com/codeplace/myfirstkmmapp/Greeting.kt
+++ shared/src/commonMain/kotlin/com/codeplace/myfirstkmmapp/Greeting.kt
@@ -5,8 +5,9 @@ import kotlin.random.Random
 class Greeting {
     private val platform: Platform = getPlatform()
 
-    fun greet(): String {
-        val firstWord = if(Random.nextBoolean()) "Hi" else "Hello"
-        return "$firstWord guess what this is! > ${platform.name.reversed()} "
+    fun greet():List<String> = buildList{
+        add (if(Random.nextBoolean()) "Hi" else "Hello")
+        add( "guess what this is! > ${platform.name.reversed()} ")
+
     }
 }
