feb18c33bc4faebafbca4bb883b2839680d1688a
Leonardocvaleriano <leonardoconsanivaleriano@gmail.com>
2023-12-23 14:06:35 +0000

    feat: implemented Network requests and data serialization dependencies

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
+    //for deserializing JSON responses into objects of entity classes used to process network operations.
+    kotlin("plugin.serialization") version "1.9.21"
+
 }
 
 kotlin {
@@ -11,7 +14,7 @@ kotlin {
             }
         }
     }
-    
+
     listOf(
         iosX64(),
         iosArm64(),
@@ -22,12 +25,33 @@ kotlin {
             isStatic = true
         }
     }
+    val ktorVersion = "2.3.5"
 
     sourceSets {
         commonMain.dependencies {
             //put your multiplatform dependencies here
+
+            // date time api
             implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.0")
 
+            // Courotines for write asynchronous code
+            implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+
+            // Ktor stuffs
+
+            // Ktor Client
+            implementation("io.ktor:ktor-client-core:$ktorVersion")
+            // responsible for serializing/deserializing the content in a specific format.
+            implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
+            // Instruct Ktor to use the JSON format and kotlinx.serialization as a serialization library.
+            implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
+        }
+
+        androidMain.dependencies {
+            implementation("io.ktor:ktor-client-android:$ktorVersion")
+        }
+        iosMain.dependencies {
+            implementation("io.ktor:ktor-client-darwin:$ktorVersion")
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
