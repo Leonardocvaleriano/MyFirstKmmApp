c168ac93783861dcf7d9fe4fe147ad5606f75759
Leonardocvaleriano <leonardoconsanivaleriano@gmail.com>
2023-12-23 12:55:15 +0000

    feat: customized hello world

--- shared/src/commonMain/kotlin/com/codeplace/myfirstkmmapp/Greeting.kt
+++ shared/src/commonMain/kotlin/com/codeplace/myfirstkmmapp/Greeting.kt
@@ -1,9 +1,12 @@
 package com.codeplace.myfirstkmmapp
 
+import kotlin.random.Random
+
 class Greeting {
     private val platform: Platform = getPlatform()
 
     fun greet(): String {
-        return "Hello, ${platform.name}!"
+        val firstWord = if(Random.nextBoolean()) "Hi" else "Hello"
+        return "$firstWord guess what this is! > ${platform.name.reversed()} "
     }
 }
